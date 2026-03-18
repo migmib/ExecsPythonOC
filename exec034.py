f6ede7edd186ee4141bac6d4313c4068371d198c
migmib <migmib55@gmail.com>
2026-03-18 01:31:29 -0300

    Delete exec034.py

--- exec034.py
+++
@@ -1,9 +0,0 @@
-n : int = 0 
-conta : int = 0 
-
-n = int(input("Digite um número para ver a tabuada dele:"))
-
-for ac in range (0, 11):
-    conta = n * ac
-    print (f"{n} X {ac} = {conta}")
-        
