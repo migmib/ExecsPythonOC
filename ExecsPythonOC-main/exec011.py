72a3f38322799c8410ddc9a248945ff50fe513a4
migmib <migmib55@gmail.com>
2026-03-18 01:13:55 -0300

    Delete ExecsPythonOC-main directory

--- ExecsPythonOC-main/exec011.py
+++
@@ -1,9 +0,0 @@
-import math
-
-r : float = 0
-c : float = 0
-
-r = float(input("Digite o raio do seu circulo:\n"))
-c = (2 * math.pi * r) 
-
-print ("O comprimento do seu círculo é de:\n", c, "metros")
--- ExecsPythonOC-main/exec033.py
+++
@@ -1,9 +0,0 @@
-n: int = 0 
-st: float = 0
-
-n = int(input("Digite um valor para somar:"))
-
-for ac in range (1,n+1):
-    st = st + (1/ac)
-
-print (f"a soma das frações de 1 até o denominador {n} é = {st}")    
